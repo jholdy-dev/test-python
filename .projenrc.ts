e5b0a382fd1d548a5113bdabb436ac946401414e
Jholdy Damasceno <jholdy.dev@gmail.com>
2024-01-16 01:05:06 -0300

    chore: config front in reactjs

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,43 @@
-import { typescript } from 'projen';
+import { typescript, python } from 'projen';
+import { ReactTypeScriptProject } from 'projen/lib/web';
 const project = new typescript.TypeScriptProject({
+  authorName: 'Jholdy Damasceno',
   defaultReleaseBranch: 'main',
   name: 'test-verzel',
   projenrcTs: true,
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  eslintOptions: { prettier: true, dirs: ['projects'] },
 });
+
+project.package.addField('prettier', {
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'es5',
+});
+
+new python.PythonProject({
+  name: 'Backend',
+  moduleName: 'backend',
+  authorName: 'Jholdy Damasceno',
+  authorEmail: 'jholdydamasceno@gmail.com',
+  version: '1.0.0',
+  outdir: './projects/backend/app-code',
+  parent: project,
+});
+
+const frontend = new ReactTypeScriptProject({
+  name: 'Frontend',
+  authorName: 'Jholdy Damasceno',
+  authorEmail: 'jholdydamasceno@gmail.com',
+  outdir: './projects/frontend/app-code',
+  defaultReleaseBranch: 'main',
+  parent: project,
+  eslintOptions: { prettier: true, dirs: ['projects/frontend'] },
+});
+
+frontend.package.addField('prettier', {
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+});
+
 project.synth();
